85515f2ffba096acc16182d8d557fb859cc9676f
M-Shinoda <sect011masami@gmail.com>
2022-11-13 01:23:10 +0900

    change reference https://github.com/homy-newfs8/libsesame3bt/blob/main/example/by_address/by_address.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 /*
  * libasesame3btサンプル
- * BluetoothスキャンしてSesameを探す場合
+ * SesameのBluetoothアドレスがわかっている場合
  */
-// #include <Arduino.h>
 #include <M5StickCPlus.h>
 #include <Sesame.h>
 #include <SesameClient.h>
@@ -11,19 +10,6 @@
 // 数行下で SESAME_SECRET 等を直接定義する場合は別ファイルを用意する必要はない
 #if __has_include("mysesame-config.h")
 #include "mysesame-config.h"
-#endif
-
-#if !defined(SESAME_SECRET)
-// 32文字の16進数でSesameの秘密鍵(sesame-qr-reader 結果の Secret Key)
-#define SESAME_SECRET "**REPLACE**"
-#endif
-#if !defined(SESAME_PK)
-// 128文字の16進数でSesameの公開鍵(sesame-qr-reader 結果の Public Key)
-#define SESAME_PK "**REPLACE**"
-#endif
-#if !defined(SESAME_MODEL)
-// 使用するSESAMEのモデル (sesame_3, sesame_4, sesame_cycle, sesame_bot)
-#define SESAME_MODEL Sesame::model_t::sesame_3
 #endif
 
 // 64 bytes public key of Sesame
@@ -35,98 +21,22 @@ const char* sesame_sec = SESAME_SECRET;
 
 using libsesame3bt::Sesame;
 using libsesame3bt::SesameClient;
-using libsesame3bt::SesameInfo;
-using libsesame3bt::SesameScanner;
 
 SesameClient client{};
-
-static const char*
-model_str(Sesame::model_t model) {
-	switch (model) {
-		case Sesame::model_t::sesame_3:
-			return "SESAME 3";
-		case Sesame::model_t::wifi_2:
-			return "Wi-Fi Module 2";
-		case Sesame::model_t::sesame_bot:
-			return "SESAME bot";
-		case Sesame::model_t::sesame_cycle:
-			return "SESAME Cycle";
-		case Sesame::model_t::sesame_4:
-			return "SESAME 4";
-		default:
-			return "UNKNOWN";
-	}
-}
-
-// Bluetoothスキャンを実行し、最初に見つけたSESAME向けに接続設定を実行する
-void
-scan_and_init() {
-	// SesameScannerはシングルトン
-	SesameScanner& scanner = SesameScanner::get();
-
-	Serial.println(F("Scanning 10 seconds"));
-	std::vector<SesameInfo> results;
-
-	// SesameScanner::scanはスキャン完了までブロックする
-	// コールバック関数には SesameScanner& と、スキャンによって得られた情報 SesameInfo* が渡される
-	// SesameInfo* の中身はコールバックを抜けると破壊されるので必要ならばコピーしておくこと
-	// スキャンが終了する際には SesameInfo* = nullptr で呼び出される
-	// コールバック中に _scanner.stop() を呼び出すと、そこでスキャンは終了する
-	// スキャン結果には WiFiモジュール2が含まれるが本ライブラリでは対応していない
-	// 非同期スキャンを実行する SesameScanner::scan_async()もある
-	scanner.scan(10, [&results](SesameScanner& _scanner, const SesameInfo* _info) {
-		if (_info) {  // nullptrの検査を実施
-			// 結果をコピーして results vector に格納する
-			Serial.printf_P(PSTR("model=%s,addr=%s,UUID=%s,registered=%u\n"), model_str(_info->model), _info->address.toString().c_str(),
-			                _info->uuid.toString().c_str(), _info->flags.registered);
-			results.push_back(*_info);
-		}
-		// _scanner.stop(); // スキャンを停止させたくなったらstop()を呼び出す
-	});
-	Serial.printf_P(PSTR("%u devices found\n"), results.size());
-	auto found =
-	    std::find_if(results.cbegin(), results.cend(), [](auto& it) { return it.model == SESAME_MODEL && it.flags.registered; });
-	if (found != results.cend()) {
-		Serial.printf_P(PSTR("Using %s (%s)\n"), found->uuid.toString().c_str(), model_str(found->model));
-		// 最初に見つけた SESAME_MODEL のデバイスに接続する
-		// 本サンプルでは認証用の鍵と見つかったSESAMEの組合せ確認は実施していないので、複数のSESAMEがある環境では接続に失敗することがある
-		if (!client.begin(found->address, found->model)) {
-			Serial.println(F("Failed to begin"));
-			return;
-		}
-		// SESAMEの鍵情報を設定
-		if (!client.set_keys(sesame_pk, sesame_sec)) {
-			Serial.println(F("Failed to set keys"));
-		}
-	} else {
-		Serial.println(F("No usable Sesame found"));
-	}
-}
-
-bool connected = false;
+SesameClient::Status last_status{};
 SesameClient::state_t sesame_state;
 
-// SesameClientの状態変化コールバック
-// Sesameとの切断を検知した場合は idle ステートへ遷移する
+// Sesameの状態通知コールバック
+// Sesameのつまみの位置、電圧、施錠開錠状態が通知される
+// Sesameからの通知がある毎に呼び出される(変化がある場合のみ通知されている模様)
+// Sesameの設定変更があった場合も呼び出される(はずだが、今のところ動作していない)
 void
-state_update(SesameClient& client, SesameClient::state_t state) {
-	sesame_state = state;
-	switch (state) {
-		case SesameClient::state_t::idle:  // 暇、接続していない
-			Serial.println(F("State: idle"));
-			break;
-		case SesameClient::state_t::connected:  // とりあえず接続はした
-			Serial.println(F("State: connected"));
-			break;
-		case SesameClient::state_t::authenticating:  // 認証結果待ち
-			Serial.println(F("State: authenticating"));
-			break;
-		case SesameClient::state_t::active:  // 認証完了しSesameが使える状態
-			Serial.println(F("State: active"));
-			break;
-		default:
-			Serial.printf_P(PSTR("State: Unknown(%u)\n"), static_cast<uint8_t>(state));
-			break;
+status_update(SesameClient& client, SesameClient::Status status) {
+	if (status != last_status) {
+		Serial.printf_P(PSTR("Setting lock=%d,unlock=%d\n"), status.lock_position(), status.unlock_position());
+		Serial.printf_P(PSTR("Status in_lock=%u,in_unlock=%u,pos=%d,volt=%.2f,volt_crit=%u\n"), status.in_lock(), status.in_unlock(),
+		                status.position(), status.voltage(), status.voltage_critical());
+		last_status = status;
 	}
 }
 
@@ -137,44 +47,98 @@ setup() {
 	// Bluetoothは初期化しておくこと
 	BLEDevice::init("");
 
-	// Bluetoothスキャンと接続設定
-	scan_and_init();
-	client.set_state_callback(state_update);
-
-	// connectはたまに失敗するようなので3回リトライする
-	Serial.print(F("Connecting..."));
-	connected = client.connect(3);
-
-	Serial.println(connected ? F("done") : F("failed"));
+	// Bluetoothアドレスと機種コードを設定(sesame_3, sesame_4, sesame_cycle を指定可能)
+	if (!client.begin(BLEAddress{SESAME_ADDRESS, BLE_ADDR_RANDOM}, SESAME_MODEL)) {
+		Serial.println(F("Failed to begin"));
+		return;
+	}
+	// Sesameの鍵情報を設定
+	if (!client.set_keys(sesame_pk, sesame_sec)) {
+		Serial.println(F("Failed to set keys"));
+		return;
+	}
+	// SesameClient状態コールバックを設定
+	client.set_state_callback([](auto& client, auto state) { sesame_state = state; });
+	// Sesame状態コールバックを設定
+	// (SESAME botは異なる呼び出しが必要。by_address_botを参照)
+	client.set_status_callback(status_update);
 }
 
-bool unlock_requested = false;
+static uint32_t last_operated = 0;
+int state = 0;
+int count = 0;
 
-// setup()内で接続に成功していたら unlock を一回実行する
 void
 loop() {
-	if (!connected) {
-		delay(1000);
-		return;
+	// 接続開始、認証完了待ち、開錠、施錠を順次実行する
+	switch (state) {
+		case 0:
+			if (last_operated == 0 || millis() - last_operated > 3000) {
+				count++;
+				Serial.println(F("Connecting..."));
+				// connectはたまに失敗するようなので3回リトライする
+				if (!client.connect(3)) {
+					Serial.println(F("Failed to connect, abort"));
+					state = 4;
+					return;
+				}
+				Serial.println(F("connected"));
+				last_operated = millis();
+				state = 1;
+			}
+			break;
+		case 1:
+			if (client.is_session_active()) {
+				Serial.println(F("Unlocking"));
+				// unloc(), lock()ともにコマンドの送信が成功した時点でtrueを返す
+				// 開錠、施錠されたかはstatusコールバックで確認する必要がある
+				if (!client.unlock(u8"開錠:テスト")) {
+					Serial.println(F("Failed to send unlock command"));
+				}
+				last_operated = millis();
+				if (client.get_model() == Sesame::model_t::sesame_cycle) {
+					state = 3;
+				} else {
+					state = 2;
+				}
+			} else {
+				if (client.get_state() == SesameClient::state_t::idle) {
+					Serial.println(F("Failed to authenticate"));
+					state = 4;
+				}
+			}
+			break;
+		case 2:
+			if (millis() - last_operated > 5000) {
+				Serial.println(F("Locking"));
+				if (!client.lock(u8"施錠:テスト")) {
+					Serial.println(F("Failed to send lock command"));
+				}
+				last_operated = millis();
+				state = 3;
+			}
+			break;
+		case 3:
+			if (millis() - last_operated > 3000) {
+				client.disconnect();
+				Serial.println(F("Disconnected"));
+				last_operated = millis();
+				if (count > 0) {
+					state = 4;
+				} else {
+					state = 0;
+				}
+			}
+			break;
+		case 4:
+			// テストを兼ねてデストラクタを呼び出しているが、あえて明示的に呼び出す必要はない
+			client.~SesameClient();
+			Serial.println(F("All done"));
+			state = 9999;
+			break;
+		default:
+			// nothing todo
+			break;
 	}
-	// 接続完了後に idle に遷移するのは認証エラーや切断が発生した場合(接続からやり直す必要がある)
-	if (sesame_state == SesameClient::state_t::idle) {
-		Serial.println(F("Failed to operate"));
-		connected = false;
-		// このサンプルではリトライは実装していない
-		return;
-	}
-	// 認証完了してSesameを制御可能になるまで待つ
-	if (sesame_state != SesameClient::state_t::active) {
-		delay(100);
-		return;
-	}
-	if (!unlock_requested) {
-		Serial.println(F("Unlocking"));
-		client.unlock(u8"ラベルは21バイトに収まるよう(勝手に切ります)");
-		client.disconnect();
-		Serial.println(F("Disconnected"));
-		connected = false;
-	}
-	delay(1000);
+	delay(100);
 }
